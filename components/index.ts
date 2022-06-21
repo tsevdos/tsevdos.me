7ed498883fcf3462bb16025a3c47838c8cdf16ab
John Tsevdos <tsevdosjohn@gmail.com>
2022-06-21 11:12:30 +0300

    clean start

--- components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MainHeader } from "./MainHeader/MainHeader";
-export { default as SocialButtons } from "./SocialButtons/SocialButtons";
-export { default as MainMenuMobile } from "./MainHeader/MainMenuMobile";
-export { default as Card } from "./Card/Card";
