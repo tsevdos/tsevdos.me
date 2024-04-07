41ba1c26d5cd2e4023c89b59132450d886d34474
John Tsevdos <tsevdosjohn@gmail.com>
2024-04-07 22:10:51 +0300

    feat: convert tsevdos.me to astro

---
+++ src/components/Header.tsx
@@ -0,0 +1,238 @@
+import { Fragment, useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
+
+export default function Header() {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("theme", "");
+      document.documentElement.classList.remove("dark");
+    } else {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  // Menu items array to populate both desktop and mobile links
+  const menuItems = [
+    {
+      name: "About",
+      url: "/about",
+    },
+    // {
+    //   name: "OSS",
+    //   url: "#",
+    // },
+    // {
+    //   name: "Blog",
+    //   url: "#",
+    // },
+  ];
+
+  return (
+    <div className="relative overflow-hidden bg-white dark:bg-gray-900 dark:text-gray-100">
+      <header id="page-header" className="relative flex flex-none items-center py-8">
+        <div className="container mx-auto flex items-center justify-between px-4 text-center lg:px-8 xl:max-w-7xl">
+          <div className="flex items-center">
+            <a
+              href="/"
+              className="group inline-flex items-center gap-2 text-lg font-bold tracking-wide text-gray-900 hover:text-gray-600 dark:text-gray-100 dark:hover:text-gray-300"
+            >
+              <svg
+                className="hi-mini hi-rocket-launch inline-block size-5 text-blue-600 transition duration-200 group-hover:-rotate-45 group-active:-translate-y-2 dark:text-blue-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.606 12.97a.75.75 0 01-.134 1.051 2.494 2.494 0 00-.93 2.437 2.494 2.494 0 002.437-.93.75.75 0 111.186.918 3.995 3.995 0 01-4.482 1.332.75.75 0 01-.461-.461 3.994 3.994 0 011.332-4.482.75.75 0 011.052.134z"
+                  clipRule="evenodd"
+                ></path>
+                <path
+                  fillRule="evenodd"
+                  d="M5.752 12A13.07 13.07 0 008 14.248v4.002c0 .414.336.75.75.75a5 5 0 004.797-6.414 12.984 12.984 0 005.45-10.848.75.75 0 00-.735-.735 12.984 12.984 0 00-10.849 5.45A5 5 0 001 11.25c.001.414.337.75.751.75h4.002zM13 9a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span>Tsevdos.me</span>
+            </a>
+          </div>
+
+          <div className="flex items-center">
+            <nav className="hidden space-x-1 md:space-x-6 lg:flex">
+              {menuItems.map((item) => (
+                <a
+                  className="text-md p-2 self-center font-semibold text-gray-950 decoration-blue-500 decoration-2 underline-offset-8 hover:underline dark:text-gray-50 dark:decoration-blue-400"
+                  key={item.name}
+                  href={item.url}
+                >
+                  <span>{item.name}</span>
+                </a>
+              ))}
+              <button
+                onClick={toggleTheme}
+                className="h-10 w-10 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <svg className="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+                <svg className="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </nav>
+            <div className="lg:hidden">
+              <button
+                onClick={() => setMobileNavOpen(true)}
+                type="button"
+                className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
+                aria-controls="tkMobileNav"
+              >
+                <svg
+                  className="hi-mini hi-bars-3 inline-block size-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          as={Fragment}
+          show={mobileNavOpen}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-050 translate-x-full"
+          enterTo="opacity-100 translate-x-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-x-0"
+          leaveTo="opacity-50 translate-x-full"
+        >
+          <nav
+            id="tkMobileNav"
+            className="fixed bottom-0 right-0 top-0 z-50 w-72 overflow-auto bg-white/95 shadow-lg dark:bg-gray-800/95 lg:hidden"
+            tabIndex={-1}
+            aria-labelledby="tkMobileNavLabel"
+            aria-modal="true"
+            role="dialog"
+          >
+            <div className="flex items-center justify-between p-6">
+              <a
+                id="tkMobileNavLabel"
+                href="/"
+                className="group inline-flex items-center space-x-2 text-lg font-bold tracking-wide text-gray-900 hover:text-gray-600 dark:text-gray-100 dark:hover:text-gray-300"
+              >
+                <svg
+                  className="hi-mini hi-rocket-launch inline-block size-5 text-blue-600 transition duration-200 group-hover:-rotate-45 group-active:-translate-y-2 dark:text-blue-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.606 12.97a.75.75 0 01-.134 1.051 2.494 2.494 0 00-.93 2.437 2.494 2.494 0 002.437-.93.75.75 0 111.186.918 3.995 3.995 0 01-4.482 1.332.75.75 0 01-.461-.461 3.994 3.994 0 011.332-4.482.75.75 0 011.052.134z"
+                    clipRule="evenodd"
+                  ></path>
+                  <path
+                    fillRule="evenodd"
+                    d="M5.752 12A13.07 13.07 0 008 14.248v4.002c0 .414.336.75.75.75a5 5 0 004.797-6.414 12.984 12.984 0 005.45-10.848.75.75 0 00-.735-.735 12.984 12.984 0 00-10.849 5.45A5 5 0 001 11.25c.001.414.337.75.751.75h4.002zM13 9a2 2 0 100-4 2 2 0 000 4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Tsevdos.me</span>
+              </a>
+
+              <button
+                onClick={toggleTheme}
+                className="h-10 w-10 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <svg className="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+                <svg className="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+
+              <button
+                onClick={() => setMobileNavOpen(false)}
+                type="button"
+                className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
+              >
+                <svg
+                  className="hi-mini hi-x-mark -mx-0.5 inline-block size-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                </svg>
+              </button>
+            </div>
+            <div className="h-px bg-gray-200/75 dark:bg-gray-700/75" />
+
+            <nav className="flex flex-col gap-2 px-6 py-5">
+              {menuItems.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.url}
+                  className="py-1 text-sm font-semibold text-gray-900 hover:text-blue-600 dark:text-gray-100 dark:hover:text-blue-400"
+                >
+                  <span>{item.name}</span>
+                </a>
+              ))}
+            </nav>
+          </nav>
+        </Transition>
+
+        <Transition
+          as={Fragment}
+          show={mobileNavOpen}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            onClick={() => setMobileNavOpen(false)}
+            className="fixed inset-0 z-40 bg-gray-900 bg-opacity-20 backdrop-blur-sm will-change-auto dark:bg-opacity-80 lg:hidden"
+          />
+        </Transition>
+      </header>
+    </div>
+  );
+}
