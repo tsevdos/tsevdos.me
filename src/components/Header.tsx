7288020cd3265f884e4d2d6ae7fe9363cb32a669
John Tsevdos <tsevdosjohn@gmail.com>
2024-09-16 17:02:15 +0300

    feat: update packages and performance / accessibility fixes (#5)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -86,6 +86,7 @@ export default function Header() {
               <button
                 onClick={toggleTheme}
                 className="h-10 w-10 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+                aria-label="Toggle theme"
               >
                 <svg className="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
@@ -104,14 +105,15 @@ export default function Header() {
                 onClick={() => setMobileNavOpen(true)}
                 type="button"
                 className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
-                aria-controls="tkMobileNav"
+                aria-controls="mobile-nav"
+                aria-label="Open mobile navigation"
+                aria-expanded={mobileNavOpen}
               >
                 <svg
                   className="hi-mini hi-bars-3 inline-block size-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
-                  aria-hidden="true"
                 >
                   <path
                     fillRule="evenodd"
@@ -135,16 +137,16 @@ export default function Header() {
           leaveTo="opacity-50 translate-x-full"
         >
           <nav
-            id="tkMobileNav"
+            id="mobile-nav"
             className="fixed bottom-0 right-0 top-0 z-50 w-72 overflow-auto bg-white/95 shadow-lg dark:bg-gray-800/95 lg:hidden"
             tabIndex={-1}
-            aria-labelledby="tkMobileNavLabel"
-            aria-modal="true"
+            aria-labelledby="mobile-nav-label"
+            // aria-modal="true"
             role="dialog"
           >
             <div className="flex items-center justify-between p-6">
               <a
-                id="tkMobileNavLabel"
+                id="mobile-nav-label"
                 href="/"
                 className="group inline-flex items-center space-x-2 text-lg font-bold tracking-wide text-gray-900 hover:text-gray-600 dark:text-gray-100 dark:hover:text-gray-300"
               >
@@ -173,11 +175,22 @@ export default function Header() {
                 onClick={toggleTheme}
                 type="button"
                 className="h-10 w-10 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+                aria-label="Toggle theme"
               >
-                <svg className="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+                <svg
+                  className="fill-violet-700 block dark:hidden"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
                   <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                 </svg>
-                <svg className="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+                <svg
+                  className="fill-yellow-500 hidden dark:block"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
                   <path
                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                     fillRule="evenodd"
@@ -187,6 +200,8 @@ export default function Header() {
               </button>
 
               <button
+                id="toggle-main-nav"
+                aria-label="Close mobile navigation"
                 onClick={() => setMobileNavOpen(false)}
                 type="button"
                 className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
@@ -230,7 +245,14 @@ export default function Header() {
         >
           <div
             onClick={() => setMobileNavOpen(false)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                setMobileNavOpen(false);
+              }
+            }}
             role="button"
+            tabIndex={0}
+            aria-label="Close mobile navigation"
             className="fixed inset-0 z-40 bg-gray-900 bg-opacity-20 backdrop-blur-sm will-change-auto dark:bg-opacity-80 lg:hidden"
           />
         </Transition>
